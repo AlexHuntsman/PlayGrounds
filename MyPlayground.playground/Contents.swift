f28c1b4ec26cd3659b3f39ae15ee921ed5044339
AlexHuntsman <ahun0442@740s-pl-118.local>
2015-12-07 09:55:58 -0700

    added examples of methods and a class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,57 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var moon = "🌚🌚🌚"
+print(moon)
+
+//Method in swift
+func simpleMethod(numberOfThings : Int) -> Int
+{
+    if(numberOfThings > 5)
+    {
+        return 100
+    }
+    else if(numberOfThings < -20)
+    {
+        return -20
+    }
+    else
+    {
+        return numberOfThings
+    }
+}
+
+print(simpleMethod(5))
+print(simpleMethod(-45))
+
+//Classes in Swift
+class SimpleClass
+{
+    //Decralation section
+    var name :String
+    var age :Int
+    var isTired :Bool
+    
+    init()
+    {
+        self.name = String()
+        self.age = -99
+        self.isTired = true
+    }
+    
+    convenience init(name : String)
+    {
+        self.init()
+        self.name = name
+    }
+    convenience init(name :String, age :Int)
+    {
+        self.init()
+        self.name = name
+        self.age = age
+    }
+}
+var someInstance = SimpleClass()
+print(someInstance.name)
+var otherInstace = SimpleClass(name: "derf")
+print(otherInstace.name)
